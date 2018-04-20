866d02b6202de311e4c5817c07d793681d35146f
kotaro0522 <gemini.522.kotaro@gmail.com>
2018-04-20 19:03:30 +0900

    first commit and add a script to replace tab with 2 spaces

---
+++ replace_tab.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script replaces all each tab with 2 spaces in the files under the current directory.
+
+for file in `find . -type f | sed -e "/replace_tab.sh/d"`; do
+  cat $file | awk '{gsub(/\t/,"  "); print $0}' > `date +"%Y%m%d%H%M%S"`_tempfile
+  DIFF=$(diff $file `date +"%Y%m%d%H"`*_tempfile)
+  if [ -n "$DIFF" ]; then
+    mv -i `date +"%Y%m%d%H%M%S"`_tempfile $file
+  fi
+  rm `date +"%Y%m%d%H"`*_tempfile
+done
